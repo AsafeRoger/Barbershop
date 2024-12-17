4bd1bea292a2feb6bd4adfc3ebe567dab66b3304
Asafe Roger <134728635+AsafeRoger@users.noreply.github.com>
2024-12-16 21:41:29 -0300

    Add files via upload

---
+++ barber/js/login.js
@@ -0,0 +1,6 @@
+
+document.getElementById('login-form').addEventListener('submit', function(enviar) {
+    enviar.preventDefault();
+    window.location.href = '/barber/html/agendamentos.html';
+});
+
